f379d1b8e2a90c4302085ab8be3442401a8d8837
Thifhelimbilu MULABISANA <tmulabisana@gmail.com>
2025-06-03 14:09:57 +0200

    Add files via upload

---
+++ conversions.py
@@ -0,0 +1,23 @@
+#Declare the folowing numbers in separate lines: 99.23, 23 and 150
+#Declare the following string on a separate line: "100"
+#Convert 99.23 into an interger
+#Convert 23 into a float
+#Convert 150 into a string
+#Convert "100" into an interger
+#Print out all variables on separate line 
+
+#************ Compulsory Task 3 ************
+num1 = 99.23
+num2 = 23
+num3 = 150
+string1 = "100"
+
+num1_int = int(num1)
+num2_dec = float(num2)
+num3_str = str(num3)
+string1_int = int(string1)
+
+print(num1_int)
+print(num2_dec)
+print(num3_str)
+print(string1_int)
