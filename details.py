f96a77ae5a0ecc3ca41dd4d9bc19ec87ab09b69d
Thifhelimbilu MULABISANA <tmulabisana@gmail.com>
2025-06-03 13:34:02 +0200

    Add files via upload

---
+++ details.py
@@ -0,0 +1,12 @@
+#Request user's name 
+#Request user's age
+#Request user's house number
+#Request user's street name
+#Print user's details in a sentence 
+
+#************ Compulsory Task 2 ************
+user_name = input("What is your name? ")
+user_age = input("How old are you?: ")
+user_house_num = input("Please enter your house number: ")
+user_street = input("Please enter your street name: ")
+print("This is {}. She is {} years old and her physical address is house number {} on {}.".format(user_name, user_age, user_house_num, user_street))
